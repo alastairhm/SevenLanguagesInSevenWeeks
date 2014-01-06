da2b9d901ec2e17e8f7e562e438b8120dff236a2
alastairhm <alastair@montgomery.me.uk>
2014-01-06 15:27:24 +0000

    Ruby Exercises

---
+++ ruby/day1.rb
@@ -0,0 +1,14 @@
+#Ruby Day 1 Exercises
+
+#Print String "Hello World"
+puts "Hello World"
+
+#Print index of the word "Ruby"
+puts "Hello, Ruby".index("Ruby")
+
+#Print name ten time with changing number
+for i in 1..10 do
+	puts "Alastair #{i}"
+end
+
+#ruby day1.rb
